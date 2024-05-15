c36c2a3b06e45f2064e41244ab8865bf8882df46
BaraiyaKuldip <baraiyakuldip302@gmail.com>
2024-05-15 07:15:53 +0400

    exercise

---
+++ src/exercise1.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+var output = (
+<div>
+    <h1 align='center' >RESUME</h1>
+<br></br> PERSONAL INFORMATION : <br></br> <br></br>
+
+    NAME      :- KULDIP <br></br>
+    SURNAME   :- BARAIYA <br></br>
+    AGE       :- 19 <br></br>
+    GENDER    :- MALE <br></br>
+    BIRTHDATE :- 07/09/2004 <br></br>
+    RELIGION  :- HINDU <br></br>
+    CAST      :- OBC <br></br>
+    EDUCATION :- HSC <br></br>
+
+    <br></br> FAMILY DETAILS : <br></br> <br></br>
+
+    FATHER NAME         :- PRAKASHBHAI <br></br>
+    MOTHER NAME         :- DAXABEN <br></br>
+    CONTACT NUMBER      :- 9313759302 <br></br>
+    E-MAIL ADDRESS      :- BARAIYAKULDIP302@GMAIL.COM <br></br>
+    RESIDENTIAL ADDRESS :- STREET NO.4, LAXMINAGAR, GHOGHA ROAD, BHAVNAGAR, 364001. <br></br>
+    
+</div>  
+);
+root.render(output);
---
+++ src/exersice2.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+var output = (
+<div>
+  <h1 align='center'>Table of 10</h1>
+<table align='center'>
+  <tr>
+    <td>
+      10 1 10
+    </td>
+    <td>
+      10 2 20
+    </td>
+    <td>
+      10 3 30
+    </td>
+    <td>
+      10 4 40
+    </td>
+    <td>
+      10 5 50
+    </td>
+    <td>
+      10 6 60
+    </td>
+    <td>
+      10 7 70
+    </td>
+    <td>
+      10 8 80
+    </td>
+    <td>
+      10 9 90
+    </td>
+    <td>
+      10 10 100
+    </td>
+  </tr>
+</table>
+
+</div>  
+);
+root.render(output);
